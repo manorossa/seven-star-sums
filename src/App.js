86b8dd5f2e699585e54c59b06a79995f3e3512ae
Mano Mylvaganam <mano@foundation-gd.co.uk>
2018-02-17 15:06:28 +0000

    Add functions to set up the game and the question

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     super(props);
     this.state = {
       showSplash: true,
-      usedNums: [],
+      possibleNums: [],
       baseNum: 20,
       num1: null,
       num2: '?',
@@ -24,33 +24,55 @@ class App extends Component {
     };
   }
 
+  // Function to create a random number. Will be used throughout app
   getRandomNumber = (base) => {
     return Math.floor(Math.random() * Math.floor(base));
   };
 
+  // Function to define the sum to be solved. For addition and subtraction
+  // Runs at the beginning of the game, and every time the next question button is pressed
   defineSum = () => {
-    let maxNum = this.state.baseNum - 2;
-    let number3 = this.getRandomNumber(maxNum) + 1;
-    const newUsedNums = [...this.state.usedNums, number3];
+    let maxNum = this.state.possibleNums.length;
+    let randomNumIndex = this.getRandomNumber(maxNum);
+    let randomNum = this.state.possibleNums[randomNumIndex];
+    console.log(`maxNum is ${maxNum}, randomNum is ${randomNum}, randomNumIndex is ${randomNumIndex}`)
+    const possibleNums = [...this.state.possibleNums].filter( val => val !== randomNum );
+
     this.setState( { 
-      usedNums: newUsedNums, 
-      num1: number3,
-      num3: this.state.baseNum
+      num1: randomNum,
+      num3: this.state.baseNum,
+      possibleNums: possibleNums
     } )
   };
 
+  // Function to define the possible answers to a sum. For addition and subtraction.
+  // Runs once at the beginning of the game, then calls the defineSum function as a callback
+  definePossibleNums = () => {
+    let newNums = [];
+    for (let i = 1; i < this.state.baseNum; i++) {
+      newNums.push(i);
+    }
+    const possibleNums = [...this.state.possibleNums].concat(newNums);
+
+    this.setState( { 
+      possibleNums: possibleNums, 
+    }, () => {this.defineSum()});
+  }
+
+  // Function to start the game and hide the splash screen.
+  // Can be expanded to start different games (addition, subtraction, times table)
   startGameHandler() {
-    this.defineSum();
+    this.definePossibleNums();     
     this.setState({
       showSplash: false
-    })
+    });
   }
 
   render() {
     return (
       <div className="App">
         {this.state.showSplash ? <Splashscreen 
-          startgame={() => this.startGameHandler()}
+          startgame={() => this.startGameHandler()} //need full function here to pass this to nested function
         /> : null}
         <Header />
         <Sum 
@@ -62,7 +84,7 @@ class App extends Component {
           />
         <Answer />
         <Check />
-        <Result />
+        <Result nextQ={this.defineSum}/>
         <Score />
       </div>
     );
