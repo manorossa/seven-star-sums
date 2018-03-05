67dd723b7584fd94fed1efdf33c6abb8f98236a0
Mano Mylvaganam <mano@foundation-gd.co.uk>
2018-03-05 21:24:57 +0000

    Add stars to show the score
    Create a star SVG. Add an array of stars, and show the score as different coloured stars.

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Star from './Star';
 
     const Score = ({displayScore}) => {
+        let stars = [];
+        for (let i=1; i <= 7; i++) {
+            stars.push(<Star
+                key={i}
+                fill={i <= displayScore ? '#fff100' : '#ccc'}
+                stroke={i <= displayScore ? '#ff9b00' : '#888'}
+                />)
+        }
         return (
             <div className='container'>
-                <div>Your score is... {displayScore}</div>
+                <div>Your score is...</div>
+                <div>{stars}</div>
             </div>
         )
     }
