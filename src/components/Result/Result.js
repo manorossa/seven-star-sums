c63721455942cac91f3b5d69f493e5627fa47be7
Mano Mylvaganam <mano@foundation-gd.co.uk>
2018-03-24 22:36:26 +0000

    result enhancements
    If this is the first star, say 'have a star'. Otherwise 'another star'.
    If the answer is wrong, show the correct answer.

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -2,8 +2,9 @@ import React from 'react';
 import '../Answers/Answers.css';
 import './Result.css';
 
-    const Result = ({nextQ, rightWrong}) => {
+    const Result = ({nextQ, rightWrong, score, correctAns}) => {
         let borderStyle = rightWrong ? 'green-border' : 'red-border';
+        let starNum = score === 1 ? 'a' : 'another';
 
         return (
             <div className={`answer-strip ${borderStyle}`}>
@@ -11,8 +12,8 @@ import './Result.css';
                     <div className='result-container'>
                         <h4 className='white-text'>
                         {rightWrong ? 
-                        'Yay, you got the sum right! Have another star!'
-                        : 'Unlucky! That\'s the wrong answer. Try another sum.' }
+                        `Yay, you got the sum right! Have ${starNum} star!`
+                        : `Unlucky! The correct answer was ${correctAns}. Try another sum.` }
                         </h4>
                     </div>
                     <button className='horizontal-button' onClick={nextQ}>Next question</button>
