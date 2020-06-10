b5fac3a135363fe237f66a539b24066e8400cb70
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-06-10 16:36:32 +0100

    Convert Splashscreen component to TS

---
+++ src/components/Splashscreen/Splashscreen.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Splashscreen.css';
+
+interface SplashscreenProps {
+    startgame(): void;
+    resetgame(): void;
+    status: string;
+}
+
+const Splashscreen: React.FC<SplashscreenProps> = ({startgame, resetgame, status}) => {
+    let splash: JSX.Element = <div></div>;
+    
+    switch (status) {
+        case 'startGame':
+            splash = <button className='horizontal-button' onClick={startgame}>Start the sums!</button>
+            break;
+        case 'endWin':
+            splash =    <div>
+                        <h3>Well done! You&rsquo;ve got seven stars!</h3>
+                        <button className='horizontal-button' onClick={resetgame}>Play again!</button>
+                        </div>
+            break;
+        case 'endLose':
+            splash =    <div>
+                        <h3>Unlucky! You&rsquo;ve run out lives...</h3>
+                        <button className='horizontal-button' onClick={resetgame}>Try again!</button>
+                        </div>
+            break;
+        default:
+            splash = <h3>Please refresh the page.</h3>
+        }
+
+    return (
+        <div className='screen'>
+            <div className='splash'>
+                <h1>Seven Star Sums</h1>
+                {splash}
+            </div>
+        </div>
+    )
+}
+
+export default Splashscreen;
