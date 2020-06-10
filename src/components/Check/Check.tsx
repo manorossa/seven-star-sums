97dd1e35416324c9fd6052adb4d3b1da2f16f929
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-06-10 15:34:18 +0100

    Convert Check component to TS

---
+++ src/components/Check/Check.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../Answers/Answers.css'
+import withAnimation from '../../HOCs/withAnimation/withAnimation';
+
+interface CheckProps {
+    yesClicked(): void;
+    noClicked(): void;
+}
+
+const Check: React.FC<CheckProps> = ({yesClicked, noClicked}) => {
+
+    return (
+        <div className='container answer-container flex-order--3'>
+            <div><h4 className='white-text'>Are you sure?</h4></div>
+            <button className='horizontal-button' onClick={yesClicked}>Yes</button>
+            <button className='horizontal-button' onClick={noClicked}>No</button>
+        </div>
+    )
+}
+
+export default withAnimation(Check, 'confirmAnswer') ;
