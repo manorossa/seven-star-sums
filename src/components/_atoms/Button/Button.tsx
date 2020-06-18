023564111f81614d2a680f2f1401986e9f49b205
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-06-18 17:18:50 +0100

    Extend Button component to allow for more than one modifier

--- src/components/_atoms/Button/Button.tsx
+++ src/components/_atoms/Button/Button.tsx
@@ -3,12 +3,19 @@ import './Button.css';
 
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   type: 'button' | 'submit' | 'reset';
-  modifier: string;
   handler(): void;
+  modifiers?: string | string[];
 }
 
-const Button: React.FC<ButtonProps> = ({ type, modifier, handler }) => {
-  const CSSclass = `btn btn--${modifier}`;
+const Button: React.FC<ButtonProps> = ({ type, handler, modifiers }) => {
+  let CSSclass = '';
+  if (typeof modifiers === 'string') {
+    CSSclass = `btn btn--${modifiers}`;
+  }
+  if (typeof modifiers === 'object' && modifiers !== null) {
+    const expandedModifiers = modifiers.map((modifier) => `btn--${modifier}`).join(' ');
+    CSSclass = `btn ${expandedModifiers}`;
+  }
 
   return (
     <button type={type} className={CSSclass} onClick={handler}>
