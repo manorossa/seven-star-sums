a46f66259727aaa56e9c31361940b0b891423c86
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-06-22 15:46:49 +0100

    Set up initial styles and mark up for settings sheet

--- src/components/SettingsSheet/SettingsSheet.tsx
+++ src/components/SettingsSheet/SettingsSheet.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import Button from '../../UI/atoms/Button/Button';
+import './SettingsSheet.css';
 
 const SettingsSheet: React.FC = () => {
   const modifiers = 'horizontal';
   return (
     <div className="sheet--settings">
       <h3>What type of sums do you want to play today?</h3>
-      <Button type="button" handler={() => alert('pairs')} modifiers={modifiers}>
-        Number pairs
-      </Button>
-      <Button type="button" handler={() => alert('tables')} modifiers={modifiers}>
-        Times tables
-      </Button>
+      <div className="settings__button-container">
+        <Button type="button" handler={() => alert('pairs')} modifiers={modifiers}>
+          Number pairs
+        </Button>
+        <Button type="button" handler={() => alert('tables')} modifiers={modifiers}>
+          Times tables
+        </Button>
+      </div>
     </div>
   );
 };
