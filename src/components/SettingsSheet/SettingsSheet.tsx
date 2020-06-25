2c736367df3bf0eb7deb77186810440849c0097d
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-06-25 12:34:16 +0100

    Add state for where we are in the settings process

--- src/components/SettingsSheet/SettingsSheet.tsx
+++ src/components/SettingsSheet/SettingsSheet.tsx
@@ -8,6 +8,7 @@ interface SettingsSheetProps {
 }
 
 const SettingsSheet: React.FC<SettingsSheetProps> = ({ handleSettings }) => {
+  const [settingStatus, setSettingStatus] = useState(1);
   const [operator, setOperator] = useState('+' as SettingsPayload['operator']);
   const [baseNum, setBaseNum] = useState(2);
 
@@ -17,11 +18,11 @@ const SettingsSheet: React.FC<SettingsSheetProps> = ({ handleSettings }) => {
   const tableOptions = [2, 3, 4, 5, 8, 10];
   const pairOptions = [10, 20];
 
-  const buttonMap = (options: number[]) =>
+  const buttonMap = (options: number[]): JSX.Element[] =>
     options.map(
       (option): JSX.Element => {
         return (
-          <Button type="button" handler={() => setBaseNum(option)} modifiers={smallRoundButtons}>
+          <Button type="button" handler={(): void => setBaseNum(option)} modifiers={smallRoundButtons}>
             {option}
           </Button>
         );
@@ -31,37 +32,45 @@ const SettingsSheet: React.FC<SettingsSheetProps> = ({ handleSettings }) => {
   const pairMap = buttonMap(pairOptions);
   const tableMap = buttonMap(tableOptions);
 
+  // PANEL HANDLERS START
+  const panel1Handler = (chosenOperator: SettingsPayload['operator']): void => {
+    setSettingStatus(2);
+    setOperator(chosenOperator);
+  };
+
   return (
     <div className="sheet--settings">
       <div className="settings__panel settings__panel--1">
         <h3>What type of sums do you want to play today?</h3>
         <div className="settings__button-container">
-          <Button type="button" handler={(): void => setOperator('+')} modifiers={horizButtons}>
+          <Button type="button" handler={(): void => panel1Handler('+')} modifiers={horizButtons}>
             Number pairs
           </Button>
-          <Button type="button" handler={(): void => setOperator('x')} modifiers={horizButtons}>
+          <Button type="button" handler={(): void => panel1Handler('x')} modifiers={horizButtons}>
             Times tables
           </Button>
         </div>
       </div>
-      <div className="settings__panel settings__panel--2">
-        {!!pairs && (
-          <>
-            <h3>Choose your number pair:</h3>
-            <div className="settings__button-container">{pairMap}</div>
-          </>
-        )}
-        {!pairs && (
-          <>
-            <h3>Choose your times tables:</h3>
-            <div className="settings__button-container">{tableMap}</div>
-            <p>
-              Base number is:
-              {baseNum}
-            </p>
-          </>
-        )}
-      </div>
+      {settingStatus > 1 && (
+        <div className="settings__panel settings__panel--2">
+          {!!pairs && (
+            <>
+              <h3>Choose your number pair:</h3>
+              <div className="settings__button-container settings__button-container--small">{pairMap}</div>
+            </>
+          )}
+          {!pairs && (
+            <>
+              <h3>Choose your times tables:</h3>
+              <div className="settings__button-container">{tableMap}</div>
+              <p>
+                Base number is:
+                {baseNum}
+              </p>
+            </>
+          )}
+        </div>
+      )}
       <div className="settings__panel settings__panel--4">
         <Button type="button" handler={(): void => handleSettings({ baseNum, operator })} modifiers={horizButtons}>
           Start the sums!
