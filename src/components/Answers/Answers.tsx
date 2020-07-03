60ecf3497221a7e14c2a3ca362f24ec53820e6c6
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-07-03 14:53:41 +0100

    Change Answers component to use context rather than state

--- src/components/Answers/Answers.tsx
+++ src/components/Answers/Answers.tsx
@@ -2,20 +2,19 @@ import React, { useContext } from 'react';
 import Answer from './Answer/Answer';
 import './Answers.css';
 import withAnimation from '../../HOCs/withAnimation/withAnimation';
-import { AppState } from '../../types/types';
 import StatusContext from '../../context/StatusContext';
+import AnswerContext from '../../context/AnswerContext';
 
-interface AnswersProps extends React.HTMLAttributes<HTMLDivElement> {
-  answers: AppState['possibleAns'];
-  clicked(answer: number): void;
-}
-
-const Answers: React.FC<AnswersProps> = ({ answers, clicked }) => {
-  const answerMap = answers.map(
-    (answer): JSX.Element => {
-      return <Answer key={`answer-${answer}`} value={answer} clicked={(): void => clicked(answer)} />;
-    }
-  );
+const Answers: React.FC = () => {
+  const { possibleAns, answerClickHandler } = useContext(AnswerContext);
+  const answerMap =
+    possibleAns &&
+    answerClickHandler &&
+    possibleAns.map(
+      (answer): JSX.Element => {
+        return <Answer key={`answer-${answer}`} value={answer} clicked={(): void => answerClickHandler(answer)} />;
+      }
+    );
 
   const { gameStatus } = useContext(StatusContext);
   const order = gameStatus === 'showSum' ? '1' : '4';
