e1193336d4b46d1e854a096ee3465ced2dcfd3ce
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-07-09 16:01:08 +0100

    Rework AnswerContext to set state and include a consumer hook

---
+++ src/context/AnswerContext.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode, useState, useMemo, useContext } from 'react';
+import { AppState } from '../types/types';
+
+type AnswerContextProps = {
+  possibleAns: AppState['possibleAns'];
+  answerClickHandler?(value: number): void;
+  noCheckHandler?(): void;
+  yesCheckHandler?(): void;
+  nextQuestionHandler?(): void;
+  correctAns: AppState['correctAns'];
+  setPossibleAns: React.Dispatch<React.SetStateAction<AppState['possibleAns']>>;
+  setCorrectAns: React.Dispatch<React.SetStateAction<AppState['correctAns']>>;
+};
+
+type Props = { children: ReactNode };
+
+const AnswerContext = React.createContext<Partial<AnswerContextProps>>({});
+const { Provider } = AnswerContext;
+
+const AnswerProvider = ({ children }: Props): JSX.Element => {
+  const [possibleAns, setPossibleAns] = useState([] as number[]);
+  const [correctAns, setCorrectAns] = useState(null as AppState['correctAns']);
+
+  const answerContextValues = useMemo(() => ({ possibleAns, correctAns, setPossibleAns, setCorrectAns }), [
+    possibleAns,
+    correctAns
+  ]);
+  return <Provider value={answerContextValues}>{children}</Provider>;
+};
+
+const useAnswer = (): AnswerContextProps => {
+  const { possibleAns, correctAns, setPossibleAns, setCorrectAns } = useContext(AnswerContext);
+
+  if (
+    possibleAns === undefined ||
+    correctAns === undefined ||
+    setPossibleAns === undefined ||
+    setCorrectAns === undefined
+  ) {
+    throw new Error('useAnswer must be used within a AnswerProvider');
+  }
+
+  return { possibleAns, correctAns, setPossibleAns, setCorrectAns };
+};
+
+export { AnswerProvider, useAnswer };
