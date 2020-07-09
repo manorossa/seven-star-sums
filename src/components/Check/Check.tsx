699921f6dc5fb6c84cf006ad32189e3e03311b6a
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-07-09 12:15:35 +0100

    Move the losing life method into the check component

--- src/components/Check/Check.tsx
+++ src/components/Check/Check.tsx
@@ -3,19 +3,29 @@ import Button from '../../UI/atoms/Button/Button';
 import '../Answers/Answers.css';
 import withAnimation from '../../HOCs/withAnimation/withAnimation';
 import AnswerContext from '../../context/AnswerContext';
+import SumContext from '../../context/SumContext';
+import { useScore } from '../../context/ScoreContext';
 
 const Check: React.FC = (): JSX.Element => {
-  const { noCheckHandler, yesCheckHandler } = useContext(AnswerContext);
+  const { noCheckHandler, yesCheckHandler, correctAns } = useContext(AnswerContext);
+  const { num2 } = useContext(SumContext);
   const modifiers = 'horizontal';
   if (noCheckHandler === undefined || yesCheckHandler === undefined) {
     throw new Error('No handlers are defined');
   }
+  const { setLivesLeft } = useScore();
+  const yesButtonHandler = (): void => {
+    yesCheckHandler();
+    if (num2 !== correctAns) {
+      setLivesLeft((prevLives) => prevLives - 1);
+    }
+  };
   return (
     <div className="container answer-container flex-order--3">
       <div>
         <h4 className="white-text">Are you sure?</h4>
       </div>
-      <Button type="button" handler={yesCheckHandler} modifiers={modifiers}>
+      <Button type="button" handler={yesButtonHandler} modifiers={modifiers}>
         Yes
       </Button>
       <Button type="button" handler={noCheckHandler} modifiers={modifiers}>
