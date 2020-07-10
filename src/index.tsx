a7274191cd25ff2fa0c0626315c30a9793e5d7f3
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-07-10 15:28:27 +0100

    Move Status provider up a level to the index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { StatusProvider } from './context/StatusContext';
 import App from './containers/App/App';
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <StatusProvider>
+    <App />
+  </StatusProvider>,
+  document.getElementById('root')
+);
 // registerServiceWorker();
