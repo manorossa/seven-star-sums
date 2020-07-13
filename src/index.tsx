b858fa719bf8eaab0492b17b61314a8657d16b0b
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-07-13 16:39:42 +0100

    Move App from container to components directory. Delete test, will be created later.

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { StatusProvider } from './context/StatusContext';
-import App from './containers/App/App';
+import App from './components/App/App';
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
