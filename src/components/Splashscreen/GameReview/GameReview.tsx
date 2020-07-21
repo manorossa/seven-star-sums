6589f5e9ad94358529ebc4acf2f480b3b915f7bc
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-07-21 16:58:19 +0100

    Correctly work out the sums for multiplication

--- src/components/Splashscreen/GameReview/GameReview.tsx
+++ src/components/Splashscreen/GameReview/GameReview.tsx
@@ -15,18 +15,20 @@ const GameReview: React.FC = () => {
   const pluralStars = score === 1 ? '' : 's';
   const pluralLives = lostLives === 1 ? 'life' : 'lives';
 
-  const finalScore = `You got ${score} star${pluralStars}.`;
+  const finalScore = `You won ${score} star${pluralStars}.`;
   const finalLives = `You lost ${lostLives} ${pluralLives}.`;
 
   const didLoseLives = lostLives > 0;
 
   const wrongSums = didLoseLives
     ? wrongAnswers.map((obj) => {
-        const missingNum = answerMethod[op1](baseNum, obj.correctAnswer as number);
-        const sum =
-          op1 === '+'
-            ? `${missingNum} + ${obj.correctAnswer} = ${baseNum}`
-            : `${missingNum} x ${baseNum} = ${obj.correctAnswer}`;
+        const isAddition = op1 === '+';
+        const missingNum = isAddition
+          ? answerMethod[op1](baseNum, obj.correctAnswer as number)
+          : (obj.correctAnswer as number) / baseNum;
+        const sum = isAddition
+          ? `${missingNum} + ${obj.correctAnswer} = ${baseNum}`
+          : `${missingNum} x ${baseNum} = ${obj.correctAnswer}`;
 
         return (
           <div className="game-review__sum-container" key={`sum-${obj.correctAnswer}`}>
