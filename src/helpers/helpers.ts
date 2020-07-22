4e1612e5e3dea90f93afe7c88a6dd4354cdef0a8
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-07-22 16:50:22 +0100

    Vary the difference between the possible answers and correct answer according to which times table is set

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -49,11 +49,12 @@ export const defineSum = (
 
   // Define correct answer, and two incorrect other possibles, +/- up to 3 to the correct answer
   const answer1 = answerMethod[op1](baseNum, randomNum);
-  let answer2 = answer1 + (getRandomNumber(3) + 1);
+  const answerVariance = op1 === '+' ? 3 : baseNum;
+  let answer2 = answer1 + (getRandomNumber(answerVariance) + 1);
   if (op1 === '+') {
     answer2 = answer2 > baseNum ? baseNum : answer2; // Don't allow random answer to be higher than the baseNum
   }
-  let answer3 = answer1 - (getRandomNumber(3) + 1);
+  let answer3 = answer1 - (getRandomNumber(answerVariance) + 1);
   answer3 = answer3 < 0 ? 0 : answer3; // Don't allow random answer to be negative
   // Put the possible answers into an array, ready to be shuffled
   const answerArray = [answer1, answer2, answer3];
