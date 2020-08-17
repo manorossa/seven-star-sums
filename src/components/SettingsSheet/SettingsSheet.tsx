6cfa7f0a729d536a443ebdc74273bbb9e7c0c616
Mano Mylvaganam <mano.mylvaganam@inviqa.com>
2020-08-17 17:07:19 +0100

    Remove commented out code

--- src/components/SettingsSheet/SettingsSheet.tsx
+++ src/components/SettingsSheet/SettingsSheet.tsx
@@ -3,6 +3,7 @@ import SettingsPanel1 from './SettingsPanels/SettingsPanel1';
 import SettingsPanel2 from './SettingsPanels/SettingsPanel2';
 import SettingsPanel3 from './SettingsPanels/SettingsPanel3';
 import SettingsPanel4 from './SettingsPanels/SettingsPanel4';
+import { buttonStyles } from './SettingsPanels/ButtonMapper';
 import Button from '../../UI/atoms/Button/Button';
 import './SettingsSheet.css';
 import { GenericFunc, SumState } from '../../types/types';
@@ -12,9 +13,11 @@ import { useScore } from '../../context/ScoreContext';
 import { getLocalSettings } from '../../helpers/helpers';
 
 const SettingsSheet: React.FC = () => {
+  // APP STATE FROM CONTEXT
   const { isLocalSettings, setGameStatus } = useStatus();
   const { setSumType, setBaseNum, setOp1 } = useSum();
   const { setTotalLives } = useScore();
+  // LOCAL STATE IN COMPONENT
   const [settingStatus, setSettingStatus] = useState(1);
   const [panelSumType, setPanelSumType] = useState('bonds' as SumState['sumType']);
   const [operator, setOperator] = useState('+' as SumState['op1']);
@@ -35,7 +38,7 @@ const SettingsSheet: React.FC = () => {
     // eslint-disable-next-line
   }, []);
 
-  // PANEL HANDLERS START
+  // PANEL HANDLERS
   const panel1Handler: GenericFunc<SumState['op1']> = (chosenType) => {
     if (isLocalSettings) {
       setPanelBaseNum(0);
@@ -73,107 +76,6 @@ const SettingsSheet: React.FC = () => {
     setDifficulty(lives);
   };
 
-  // PANEL VISIBILITY OPTIONS
-  // const panel2viz = settingStatus > 1 ? 'show' : 'hide';
-  // const panel3viz = settingStatus > 2 ? 'show' : 'hide';
-  // const panel4viz = settingStatus > 3 || isResetOperator ? 'show' : 'hide';
-  const panel5viz = (settingStatus > 4 && !isResetOperator) || settingStatus > 5 ? 'show' : 'hide';
-
-  // RENDERING LOGIC
-  const bonds = panelSumType === 'bonds';
-
-  // BUTTON STYLING
-  // const horizButtons = ['horizontal'];
-  const horizGreenButtons = ['horizontal', 'horizontal-green'];
-  // const smallRoundButtons = ['round', 'round-small', 'round-white-border'];
-
-  // const makeButtonStyles = (array1: string[]): string[][] => {
-  //   const array2 = [...array1, 'active'];
-  //   const array3 = [...array1, 'inactive'];
-  //   return [array1, array2, array3];
-  // };
-
-  // BUTTON CONTENT
-  // const typeOptions = ['bonds', 'tables'];
-  // const typeText = ['Number bonds', 'Times tables'];
-  // const bondOperatorOptions = ['+', '-'];
-  // const tableOperatorOptions = ['x', '÷'];
-  // const tableOptions = [2, 3, 4, 5, 8, 10];
-  // const bondOptions = [10, 20];
-  // const difficultyOptions = [7, 5, 3];
-  // const difficultyText = ['Medium: 7', 'Hard: 5', ' Very hard: 3'];
-
-  // CREATE BUTTON MAPS
-  // const buttonMap = (
-  //   options: OptionsMap<string | number | SumState['op1'] | SumState['sumType']>,
-  //   stateCheck: string | number,
-  //   panelNum: number,
-  //   clickHandler: GenericFunc<SumState['sumType'] & SumState['op1'] & number>,
-  //   buttonStyle: string[][],
-  //   buttonText: number[] | string[]
-  // ): JSX.Element[] =>
-  //   options.map(
-  //     (option: string | number | SumState['op1'] | SumState['sumType'], index: number): JSX.Element => {
-  //       const [butMod, butModAct, butModInact] = buttonStyle;
-  //       let buttonModifiers = butMod;
-  //       if (settingStatus > panelNum && stateCheck) {
-  //         buttonModifiers = option === stateCheck ? butModAct : butModInact;
-  //       }
-  //       return (
-  //         <Button
-  //           key={`panel-${panelNum}-${option}`}
-  //           type="button"
-  //           handler={(): void => clickHandler(option as SumState['sumType'] & SumState['op1'] & number)}
-  //           modifiers={buttonModifiers}
-  //         >
-  //           {buttonText[index]}
-  //         </Button>
-  //       );
-  //     }
-  //   );
-
-  // const typeMap = buttonMap(typeOptions, panelSumType, 1, panel1Handler, makeButtonStyles(horizButtons), typeText);
-  // const bondOperatorMap = buttonMap(
-  //   bondOperatorOptions,
-  //   operator,
-  //   2,
-  //   panel2Handler,
-  //   makeButtonStyles(horizButtons),
-  //   bondOperatorOptions
-  // );
-  // const tableOperatorMap = buttonMap(
-  //   tableOperatorOptions,
-  //   operator,
-  //   2,
-  //   panel2Handler,
-  //   makeButtonStyles(horizButtons),
-  //   tableOperatorOptions
-  // );
-  // const bondMap = buttonMap(
-  //   bondOptions,
-  //   panelBaseNum,
-  //   3,
-  //   panel3Handler,
-  //   makeButtonStyles(smallRoundButtons),
-  //   bondOptions
-  // );
-  // const tableMap = buttonMap(
-  //   tableOptions,
-  //   panelBaseNum,
-  //   3,
-  //   panel3Handler,
-  //   makeButtonStyles(smallRoundButtons),
-  //   tableOptions
-  // );
-  // const difficultyMap = buttonMap(
-  //   difficultyOptions,
-  //   difficulty,
-  //   4,
-  //   panel4Handler,
-  //   makeButtonStyles(horizButtons),
-  //   difficultyText
-  // );
-
   const finalSettings = (
     finalSumType: SumState['sumType'],
     finalBaseNum: number,
@@ -189,45 +91,17 @@ const SettingsSheet: React.FC = () => {
     window.localStorage.setItem('sevenStarSettings', JSON.stringify(allSettings));
   };
 
+  // PANEL VISIBILITY OPTIONS
+  const panel5viz = (settingStatus > 4 && !isResetOperator) || settingStatus > 5 ? 'show' : 'hide';
+
+  // RENDERING LOGIC
+  const bonds = panelSumType === 'bonds';
+
   return (
     <div className="settings__sheet">
-      {/* <div className="settings__panel settings__panel--1">
-        <h3>What type of sums do you want to play today?</h3>
-        <div className="settings__button-container">{typeMap}</div>
-      </div> */}
       <SettingsPanel1 stateChecker={panelSumType} handler={panel1Handler} status={settingStatus} />
-      {/* <div className={`settings__panel settings__panel--1 settings__panel--${panel2viz}`}>
-        {bonds ? (
-          <>
-            <h3>Do you want to do adding or taking away?</h3>
-            <div className="settings__button-container">{bondOperatorMap}</div>
-          </>
-        ) : (
-          <>
-            <h3>Do you want to do multiplication or division?</h3>
-            <div className="settings__button-container">{tableOperatorMap}</div>
-          </>
-        )}
-      </div> */}
       <SettingsPanel2 stateChecker={operator} handler={panel2Handler} status={settingStatus} isBonds={bonds} />
-      {/* <div className={`settings__panel settings__panel--2 settings__panel--${panel3viz}`}>
-        {bonds ? (
-          <>
-            <h3>Choose your bond number:</h3>
-            <div className="settings__button-container settings__button-container--small">{bondMap}</div>
-          </>
-        ) : (
-          <>
-            <h3>Choose your times table:</h3>
-            <div className="settings__button-container">{tableMap}</div>
-          </>
-        )}
-      </div> */}
       <SettingsPanel3 stateChecker={panelBaseNum} handler={panel3Handler} status={settingStatus} isBonds={bonds} />
-      {/* <div className={`settings__panel settings__panel--3 settings__panel--${panel4viz}`}>
-        <h3>How many lives do you want to have?</h3>
-        <div className="settings__button-container">{difficultyMap}</div>
-      </div> */}
       <SettingsPanel4
         stateChecker={difficulty}
         handler={panel4Handler}
@@ -238,7 +112,7 @@ const SettingsSheet: React.FC = () => {
         <Button
           type="button"
           handler={(): void => finalSettings(panelSumType, panelBaseNum, operator, difficulty)}
-          modifiers={horizGreenButtons}
+          modifiers={buttonStyles.horizGreen}
         >
           Start the sums!
         </Button>
